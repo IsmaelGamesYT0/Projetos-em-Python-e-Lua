c7fa25a8261e21a3afcf7f58cf04fa0938348bce
Ismael <memessr83@gmail.com>
2025-11-26 01:23:22 -0300

    update

--- Python/Desafios/desafio_001.py
+++ Python/Desafios/desafio_001.py
@@ -1,19 +1,3 @@
 # Crie um programa que escreva "Olá, Mundo!" na tela.
 
-# print("Olá, Mundo!")
-
-import time
-
-nome = input("Qual é o seu nome? ")
-idade = int(input("Quantos anos você tem? "))
-if idade >= 18:
-    print("Você é maior de idade, Entrada Liberada!")
-elif idade < 18:
-    print("Você é menor de idade, ENTRADA PROIBIDA!")
-else:
-    print("Idade inválida!")
-
-time.sleep(2)
-print("==============================================")
-
-print("Olá, {}! Você tem {} anos.".format(nome, idade))
+print("Olá, Mundo!")
