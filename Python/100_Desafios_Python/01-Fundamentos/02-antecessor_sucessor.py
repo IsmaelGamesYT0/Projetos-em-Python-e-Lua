6d40a19bb1297e6b0de57a56a846f0a84590f5d7
Ismael <memessr83@gmail.com>
2025-12-02 13:22:13 -0300

    Adicionar novos desafios de programação: olá mundo, entrada de nome, soma, antecessor/sucessor, dobro/triplo/raiz, média de três notas, conversor de metros, tabuada, conversor de moeda e par/impar.

---
+++ Python/100_Desafios_Python/01-Fundamentos/02-antecessor_sucessor.py
@@ -0,0 +1,8 @@
+numero = int(input("Por favor, informe um número inteiro: "))
+
+antecessor = numero - 1
+sucessor = numero + 1
+
+print(f"Seu número é: {numero} \n")
+print(f"o antecessor é: {antecessor} \n")
+print(f"e o sucessor é: {sucessor}")
